2b2333cdcf82624ba5ace40aac4b81ed6ff79284
Arianna Kosten <arianna.kosten@gmail.com>
2019-06-25 23:18:44 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,10 +9,11 @@ class App < Sinatra::Base
     end
 
     post '/teams' do
+      
       @team_name = params["team"]["name"]
       @team_motto = params["team"]["motto"]
       @team_members = params["team"]["members"]
-
+      
       erb :team
     end
     
